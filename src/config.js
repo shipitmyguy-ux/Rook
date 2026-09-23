9659fd36049a32cb489975ef26a7ff1e318f35b7
shipitmyguy-ux <shipitmyguy@gmail.com>
2026-09-23 12:26:06 -0600

    Add src/config.js

---
+++ src/config.js
@@ -0,0 +1,14 @@
+export const config = Object.freeze({
+  maps: {
+    provider: "google",
+    terrainEnabled: true
+  },
+  search: {
+    minBeds: 2,
+    excludeIncomeRestricted: true,
+    excludeMobileHomes: true
+  },
+  followUp: {
+    defaultHours: 24
+  }
+});
