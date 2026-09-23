4adb8d924a58d88bd5f0c2a9714c865c2b6bf425
shipitmyguy-ux <shipitmyguy@gmail.com>
2026-09-23 16:53:14 -0600

    Configure Rook live listing endpoint

--- src/config.js
+++ src/config.js
@@ -6,7 +6,11 @@ export const config = Object.freeze({
   search: {
     minBeds: 2,
     excludeIncomeRestricted: true,
-    excludeMobileHomes: true
+    excludeMobileHomes: true,
+    location: "Fort Collins, CO"
+  },
+  listings: {
+    endpoint: "https://umvmilulnqnmeqvfoxxc.supabase.co/functions/v1/rook-listings"
   },
   followUp: {
     defaultHours: 24
