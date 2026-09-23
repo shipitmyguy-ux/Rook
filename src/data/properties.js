e74ed5b00be0570e2afb4333d7f0e983639218f2
shipitmyguy-ux <shipitmyguy@gmail.com>
2026-09-23 12:23:47 -0600

    Add src/data/properties.js

---
+++ src/data/properties.js
@@ -0,0 +1,22 @@
+export const properties = [
+  {
+    id: "timberwood",
+    label: "Timberwood",
+    address: "Fort Collins, CO",
+    type: "Rental area",
+    price: null,
+    beds: 2,
+    baths: null,
+    note: "Very good area near a school; high interest. Research surrounding apartments and townhomes."
+  },
+  {
+    id: "landing-lemay",
+    label: "The Landing at Lemay",
+    address: "Fort Collins, CO",
+    type: "Apartment",
+    price: null,
+    beds: 2,
+    baths: null,
+    note: "Shortlisted for follow-up."
+  }
+];
