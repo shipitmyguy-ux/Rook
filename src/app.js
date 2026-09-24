940d07c7538753279855120a0d85b833a13006d9
shipitmyguy-ux <shipitmyguy@gmail.com>
2026-09-23 22:04:35 -0600

    Version close zoom label module v2

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import { recordActivity, getActivity } from "./core/activity.js";
 import { nextFollowUp, markShowingRequested } from "./core/followup.js";
 import { exportRookData, parseRookBackup } from "./core/export.js";
 import { searchProviders, registerConfiguredProviders, firstImageUrl } from "./integrations/providers.js";
-import { openDirections, renderPropertyMap, renderCardMap, focusPropertyOnMap } from "./integrations/maps.js?v=detail-labels-v1";
+import { openDirections, renderPropertyMap, renderCardMap, focusPropertyOnMap } from "./integrations/maps.js?v=detail-labels-v2";
 import { googleCalendarShowingUrl } from "./integrations/calendar.js";
 import { config } from "./config.js";
 
